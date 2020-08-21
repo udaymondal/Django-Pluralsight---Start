14718791e68fa6f272c33044e6437d9fe69449aa
Uday Sankar Mondal <58623748+udaymondal@users.noreply.github.com>
2020-08-21 22:56:14 +0600

    Add files via upload

--- meeting_planner/meetings/admin.py
+++ meeting_planner/meetings/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Meeting, Person
+from .models import Meeting, Room
 
 admin.site.register(Meeting)
-admin.site.register(Person)
+admin.site.register(Room)
